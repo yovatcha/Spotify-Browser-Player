498451bbe93e75b10422f52285404813de2c9fc0
Vatcharamai <98142100+yovatcha@users.noreply.github.com>
2024-02-11 00:26:07 +0700

    [UPDATE] remove access token
    just forgot to remove my access token hahaha. Now you replace with your access token.

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -2,5 +2,5 @@
 // Replace with your Spotify access token
 
 export const getAccessToken = () => {
-    return 'BQChLsHgNZouzKutMsKnOTs-LUS44BtTZ4IDHnsrTJRWpc8FpDde-v4emXzEZtuJd5Xn2yWAP8WrVYLowVASt2kJm5erpOQXiU7enTIHb1il7Xsn8GEkjXP3pedAY4AXKQUx0RL3_dYUGRGSVBnF0Z1B4p_T6Bbd-TEh-vLapUKtrh0eKv5u0I3eYT6pXyuB27X_spdoVYHDVbipKqO7GPtJSNBt';
+    return 'YOUR_ACCESS_TOKEN_HERE';
   };

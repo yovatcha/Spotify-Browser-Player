02987580c537aa13822ca7bc43fb9f314e912b29
Vatcharamai <98142100+yovatcha@users.noreply.github.com>
2024-02-13 13:57:18 +0700

    [UPDATE] access token variable

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -2,5 +2,5 @@
 // Replace with your Spotify access token
 
 export const getAccessToken = () => {
-    return 'BQDB7_8dx3YZOGvShE0lI6RXb-KOK5FPPneWwS_VPTg1EsJSnLcuJrmsVumJySv9WFYyIBungPXHpPP6SF5rACHeKGg4XjCGnB-_3yFjh_1HUtiXg7HOrfTSXbKJmog_YRq6StOAVAC2NNqz539E3KxXXxvW-GAiAX0HPqPZg_0GUb2dwy3U9GcdrbQKCNXyp_j79_bVlM74PeTkAdZ8hUB_0PLT';
+    return 'YOUR_ACCESS_TOKEN';
   };
